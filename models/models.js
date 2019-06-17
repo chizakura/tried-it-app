cb6014984d3dfc9fe5712a2040825a7109497594
Mohammad <mshakhan1996@gmail.com>
2019-06-17 14:49:20 -0400

    models

--- models/models.js
+++ models/models.js
@@ -5,35 +5,74 @@ const db = new Sequelize({
     dialect: "postgres"
 })
 
-const Users = db.define('users',{
-    name: Sequelize.STRING,
-    userId: Sequelize.INTEGER
+const Users = db.define('users', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    userId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+})
+
+const Reviews = db.define('reviews', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    date: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    rating: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    userId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    placeId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    reviewId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    entry: {
+        type: Sequelize.TEXT
+    },
+
+
 
 })
 
-const Reviews = db.define('reviews',{
-    title: Sequelize.STRING,
-    date: Sequelize.INTEGER,
-    rating: Sequelize.INTEGER,
-    userId: Sequelize.INTEGER,
-    placeId: Sequelize.INTEGER,
-    reviewId: Sequelize.INTEGER,
-    entry: Sequelize.TEXT
-   
+const Places = db.define('places', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    category: {
+        type: Sequelize.STRING
+    },
+    placeId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    address: {
+        type: Sequelize.TEXT
+    },
 
 
 })
 
-const Places = db.define('places',{
-    name: Sequelize.STRING,
-    type: Sequelize.STRING,
-    placeId: Sequelize.INTEGER,
-    address: Sequelize.TEXT,
-
-
-})
-
-module.exports={
+Users.hasMany(Reviews)
+Reviews.belongTo(Users)
+Places.hasMany(Reviews)
+Reviews.belongTo(Places)
+module.exports = {
     db,
     Users,
     Reviews,
