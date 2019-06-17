2803c8ebc247268cbc7293e52997a259a19b6327
Linda Chin <lindapchin225@gmail.com>
2019-06-17 17:16:22 -0400

    added NewsFeed to LandingPage

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import SearchBar from './SearchBar';
+import NewsFeed from './NewsFeed';
+
+//This page includes Link to CreateUser
+//This page should import SearchBAr
+//This page should import NewsFeed
 
 class LandingPage extends Component {
     render() {
         return (
             <div>
+                <nav>
+                    <Link to="/user/create">Add User</Link>
+                </nav>
                 <h1>Tried It</h1>
+                <SearchBar/>
+                <NewsFeed/>
             </div>
         )
     }
---
+++ client/src/Components/NewsFeed.js
@@ -0,0 +1,14 @@
+// This page should show user reviews
+import React, {Component} from 'react';
+
+class NewsFeed extends Component {
+    render() {
+        return (
+            <div>
+                <p>Show feed of reviews</p>
+            </div>
+        )
+    }
+}
+
+export default NewsFeed;
