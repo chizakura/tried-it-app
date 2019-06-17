897a8fb581f80ac9ce62c1a01a30747162611911
Linda Chin <lindapchin225@gmail.com>
2019-06-17 19:50:38 -0400

    added create review form

--- client/src/Components/CreateReview.js
+++ client/src/Components/CreateReview.js
@@ -1,1 +1,35 @@
 // This page should have a form to create reviews
+import React, {Component} from 'react';
+
+class CreateReview extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Add Review Form</h1>
+                <form className="form">
+                    <div className="date">
+                        <label>Date</label>
+                        <input name="date" type="date"/>
+                    </div>
+                    <div>
+                        <label>Title</label>
+                        <input name="title" type="text"/>
+                    </div>
+                    <div>
+                        <label>Rating</label>
+                        <input name="rating" type="number"/>
+                    </div>
+                    <div>
+                        <label>Entry</label>
+                        <textarea name="entry" rows="5" cols="40"></textarea>
+                    </div>
+                    <div>
+                        <input type="submit"/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default CreateReview;
--- client/src/Components/CreateUser.js
+++ client/src/Components/CreateUser.js
@@ -1,10 +1,14 @@
 // This page should have a form to create user
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class CreateUser extends Component {
     render() {
         return (
             <div>
+                <nav>
+                    <Link to="/create/review">Add Review</Link>
+                </nav>
                 <h1>Add User Form</h1>
                 <form className="form">
                     <div>
