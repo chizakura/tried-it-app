d9567644615b3bdb2dc635257c9b57508efb2b84
Mohammad <mshakhan1996@gmail.com>
2019-06-18 18:01:13 -0400

    fixed newsfeed

--- client/src/Components/NewsFeed.js
+++ client/src/Components/NewsFeed.js
@@ -7,31 +7,43 @@ class NewsFeed extends Component {
         super(props);
 
         this.state = {
-            reviews: {}
+            reviews: []
         }
     }
-    async componentDidMount(){
+    componentDidMount(){
+        this.fetchReviews()
+    }
+
+     async fetchReviews (){
         const res = await axios.get(`http://localhost:4567/review`)
-        const reviews = res.data
+        const reviews = res.data.reviews
+        console.log(reviews)
         this.setState({
           reviews: reviews
         })
-        console.log(reviews)
     }
+
+    renderReviews() {
+        if (this.state.reviews){
+            return this.state.reviews.map(review => {
+                return (
+                     <div>
+                         <h2>{review.title}</h2>
+                         <h3>{review.rating}</h3>
+                         <h4>{review.date}</h4>
+                         <p>{review.entry}</p>
+                    </div>
+                 )})
+             }
+    }
+    
     render() {
+    
+
         return (
             <div className="news-container">
                 <div>
-                    {this.state.reviews.map(review => {
-                        return (
-                             <div>
-                                 <h2>{review.title}</h2>
-                                 <h3>{review.rating}</h3>
-                                 <h4>{review.date}</h4>
-                                 <p>{review.entry}</p>
-                            </div>
-                         )
-                     })}
+                    {this.renderReviews()}
                 </div>
             </div>
         )
