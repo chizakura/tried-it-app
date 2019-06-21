998a3ce82f27bdf67d4eca53d0cad4cc873ad353
Linda Chin <lindapchin225@gmail.com>
2019-06-20 23:19:29 -0400

    minor additions

--- server.js
+++ server.js
@@ -11,22 +11,22 @@ const path = require('path')
 app.use(cors())
 app.use(bodyParser.json())
 
+// Static hosting for built files
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.use((err, req, res, next)=>{
+    console.warn(err.stack)
+    res.status(500).json({message: err.message})
+})
+
 app.get('/', (req, res) => res.send('TriedIt'))
 app.use('/user',userRouter)
 app.use('/review',reviewRouter)
 app.use('/place',placeRouter)
-
-// Static hosting for built files
-app.use(express.static(path.join(__dirname, './client/build')));
 
 // In production, any request that doesn't match a previous route
 // should send the front-end application, which will handle the route.
 if (process.env.NODE_ENV == "production") {
     app.use('*', (req, res) => res.sendFile(path.join(__dirname, './client/build', "index.html")));
 }
-
-app.use((err, req, res, next)=>{
-    console.warn(err.stack)
-    res.status(500).json({message: err.message})
-})
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
